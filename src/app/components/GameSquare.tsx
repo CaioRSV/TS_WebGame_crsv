71763d9cda4f54b1554579c5fe9e4612b533da2b
Caio Verçosa <31866855+CaioRSV@users.noreply.github.com>
2024-01-26 19:26:21 -0300

    upgrade visual e conserto de bugs

--- src/app/components/GameSquare.tsx
+++ src/app/components/GameSquare.tsx
@@ -2,35 +2,97 @@
 import { useState, useEffect } from 'react';
 import React from 'react'
 
+import Image from "next/image";
+
 interface  infosMainPage {
   mainSprite : string,
   startVar : boolean,
-  hitbox : string
+  hitbox : any[]
 }
 
+const divStyleBG = {
+  backgroundImage: "url('/utils_images/scenario_Image.png')"
+};
+
 const GameSquare : React.FC<infosMainPage> = ({mainSprite, startVar, hitbox}) => {
-  const [divAtributes, setDivAtributes] = useState<string>("min-h-96 min-w-96 text-white bg-slate-700");
+  const [divAtributes, setDivAtributes] = useState<string>("min-h-96 min-w-96 text-white bg-slate-700 grayscale");
+
+  const [shootSign, setShootSign] = useState<string>("text-slate-200 font-bold")
+
+  const [pathMC, setPathMC] = useState<string>("/utils_images/mc_StandBy.png")
+
+  const [pathGoon, setPathGoon] = useState<string>("/utils_images/goon_StandBy.png")
 
   useEffect(()=>{
     if(startVar){
       setDivAtributes("min-h-96 min-w-96 text-white bg-blue-950")
     }
     else{
-      setDivAtributes("min-h-96 min-w-96 text-white bg-slate-700");
+      setDivAtributes("min-h-96 min-w-96 text-white bg-slate-500 grayscale");
     }
+    //---  
+    if(hitbox[2]){
+      setShootSign("text-red-600 font-bold");
+    }
+    else{
+      setShootSign("text-slate-200 font-bold");
+    }
+    //-- McIMG
+    if(mainSprite=="🔥"){
+      setPathMC("/utils_images/mc_StandBy.png")
+    }
+    else if (mainSprite=="W"){
+      setPathMC("/utils_images/mc_DodgeW.png")
+    }
+    else if (mainSprite=="A"){
+      setPathMC("/utils_images/mc_DodgeA.png")
+    }
+    else if (mainSprite=="S"){
+      setPathMC("/utils_images/mc_DodgeS.png")
+    }
+    else if (mainSprite=="D"){
+      setPathMC("/utils_images/mc_DodgeD.png")
+    }
+
+    //-- GoonIMG
+    if (hitbox[2]){
+      setPathGoon("/utils_images/goon_Shoot.png");
+    }
+    else{
+      setPathGoon("/utils_images/goon_StandBy.png");
+    }
+    
   });
 
   return (
-    <div className={divAtributes}>
-      <div className = "h-screen flex items-start justify-center absolute inset-x-0">
-        <h1 className="text-red-600 font-bold">{hitbox}</h1>
+    <div className={divAtributes} style = {divStyleBG}>
+      <div className = "h-full w-full justify-center flex text-3xl">
+        <h1 className={shootSign}>{hitbox[0]}</h1>
       </div>
       
 
-      <div className="h-screen flex items-center justify-center absolute inset-x-0 bottom-0 inset-y-4">
-        <p className="text-white">{mainSprite}</p>
-        <p></p>
-      </div>
+      <div className="h-full w-full grid grid-cols-2 gap-4 mt-40">
+      {/* flex items-center justify-center inset-x-0 bottom-0 inset-y-4 */}
+        <div className="col-span-1">
+          <Image
+                src={pathMC}
+                width={180}
+                height={180}
+                alt="sheesh"
+            />
+
+        </div>
+
+        <div className="col-span-1">
+          <Image
+                src={pathGoon}
+                width={180}
+                height={180}
+                alt="sheesh"
+            />
+        </div>
+
+      </div> 
 
     </div>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,9 @@
 import { useState, useEffect } from 'react';
 import GameSquare from "./components/GameSquare";
 
+import Image from "next/image";
+
+
 const randomNumHitbox = () => {
   return Math.floor(Math.random() * (5 - 1)) + 1; // Pegando um número aleatório de 1 a 4
 }
@@ -9,7 +12,7 @@ const randomNumHitbox = () => {
 export default function Home() {
   const [emoji, setEmoji] = useState<string>("🔥");
 
-  const [hitbox, setHitbox] = useState<any[]>(["-", 0]);
+  const [hitbox, setHitbox] = useState<any[]>(["-", 0, false]); // hotkeyToDodge, timeLimitDodge, isFiring
 
   const [startTime, setStartTime] = useState<number>(0);
 
@@ -17,17 +20,14 @@ export default function Home() {
 
   const [timeAlive, setTimeAlive] = useState<number>(0);
 
+  const [longestTimeAlive, setLongestTimeAlive] = useState<number>(0);
+
   const [startVar, setStartVar] = useState<boolean>(false);
 
+  const [cooldown, setCooldown] = useState<number>(2000);
+  
+
   const handleKeyDown = (event: KeyboardEvent) => {
-    // if (emoji=="🔥"){
-    //   setEmoji("👍");
-    // }
-    // else{
-    //   setEmoji("🔥");
-    // }
-    // setEmoji(event.key);
-    // setStartTime(Date.now());
 
     if((event.key=="Backspace")){ // Acaba o jogo
       setEmoji(event.key); // Sei q é sobrescrito só pra testar msm
@@ -39,6 +39,7 @@ export default function Home() {
     else if(event.key=="Enter"){
       if(!startVar){
         gameSwitch();
+        setEmoji("🔥");
       }      
     }
     else{ // Continua
@@ -74,7 +75,7 @@ export default function Home() {
       else if((emoji=="🔥")){
         console.log('oppanganganmastyle')
         const arrayTeclas = ['W','A','S','D']
-        setHitbox([arrayTeclas[randomNumHitbox()-1], time+2000])
+        setHitbox([arrayTeclas[randomNumHitbox()-1], time+cooldown, false])
         setEmoji("⚠️");
       }
       else{
@@ -86,16 +87,26 @@ export default function Home() {
   };
 
   const hitboxCheck = () => {
+    // Sinalizando o tiro ou sla
+    if(time>hitbox[1]-100 && time<hitbox[1]){
+      setHitbox([hitbox[0], hitbox[1], true])
+    }
+
     if((hitbox[0]!="-") && (emoji!=hitbox[0]) && (time>hitbox[1]) ){
       gameSwitch();
-      setHitbox(["-", 0])
+      setHitbox(["-", 0, false])
       setEmoji("🔥");
+      setCooldown(2000);
       console.log('ERROU')
     }
     else if ((hitbox[0]!="-") && (emoji==hitbox[0]) && (time>hitbox[1])){
-      setHitbox(["-", 0])
+      setHitbox(["-", 0, false])
       setEmoji("🔥");
-      console.log('ACERTOU!!!!!')  
+      console.log('ACERTOU!!!!!')
+      if(cooldown>300){
+        setCooldown(cooldown-50);
+      }
+      
     }
 
   };
@@ -107,6 +118,9 @@ export default function Home() {
       setStartTime(0);
       setTime(0);
       setStartVar(false);
+
+      setCooldown(2000);
+      setHitbox(["-", 0, false]);
     }
     else{
       setStartTime(Date.now());
@@ -123,6 +137,14 @@ export default function Home() {
 
     const intervalHIT = setInterval(hitboxCheck, 30)
 
+    if (timeAlive>longestTimeAlive){
+      setLongestTimeAlive(timeAlive);
+    };
+
+    if ((hitbox[0]=="-") && (startVar==true)){
+      setEmoji("🔥");
+    }
+
     return () => {
       document.removeEventListener('keydown', handleKeyDown);
       clearInterval(intervalId);
@@ -132,14 +154,42 @@ export default function Home() {
     };
   });
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        <h1>{timeAlive}</h1>
-        <GameSquare mainSprite={emoji} startVar={startVar} hitbox={hitbox[0]}/>
-        <button onClick={() => {gameSwitch()}}> -------- </button>
-        <p>{time} / Hitbox: {hitbox[0]} / {hitbox[1]}</p>
-        <p>{startVar.toString()}</p>
+    <main className="flex h-full justify-center">
+      <div className="p-24">
+        <h1 className="text-center">Melhor tempo: {longestTimeAlive}</h1>
+        <GameSquare mainSprite={emoji} startVar={startVar} hitbox={hitbox}/>
+        <p className="text-center">Tempo: {time} / Próximo tiro: {hitbox[1]}</p>
+        <p className="text-center">Rodando: {startVar.toString()}</p>
+
+        <p>____________________________________________________________________</p>
+        <h1 className="text-center">Controles: </h1>
+        <div className="grid grid-cols-2 grid-rows-2 gap-4">
+          <div className="col-span-1 row-span-1">
+            <button type="button" className="cursor-default placeholder:text-gray-900 border-gray-800  font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-gray-600 dark:text-gray-400"></button>
+            <button type="button" className="cursor-default placeholder:text-gray-900 border-gray-800 hover:bg-gray-900  font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-gray-600 dark:text-gray-400">W</button>
+            <button type="button" className="cursor-default placeholder:text-gray-900 border-gray-800  font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-gray-600 dark:text-gray-400"></button>
+          </div>
+          <div className="col-span-1 row-span-1">
+            <button type="button" className="cursor-default placeholder:text-gray-900 border-gray-800 hover:bg-red-950 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-gray-600 dark:text-gray-400">Backspace (←)</button>
+            <button type="button" className="cursor-default placeholder:text-gray-900 border-gray-800 hover:bg-green-950 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-gray-600 dark:text-gray-400">Enter</button>
+          </div>
+
+          <div className="col-span-1 row-span-1">
+            <button type="button" className="cursor-default placeholder:text-gray-900 border-gray-800 hover:bg-gray-900 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-gray-600 dark:text-gray-400">A</button>
+            <button type="button" className="cursor-default placeholder:text-gray-900 border-gray-800 hover:bg-gray-900 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-gray-600 dark:text-gray-400">S</button>
+            <button type="button" className="cursor-default placeholder:text-gray-900 border-gray-800 hover:bg-gray-900 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-gray-600 dark:text-gray-400">D</button>
+          </div>
+
+        </div>
+
+
+      </div>
+
+      <div className="absolute p-10">
+      <button className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" onClick={() => {gameSwitch()}}> On/Off </button>
+
       </div>
     </main>
+    
   );
 }
